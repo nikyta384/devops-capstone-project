d4d2f7605c592a5fd877d3e5d84735cb90e732d3
nikyta384 <nikyta384@gmail.com>
2024-02-12 08:22:21 -0500

    Added CORS headers

--- service/__init__.py
+++ service/__init__.py
@@ -9,6 +9,7 @@ from flask import Flask
 from service import config
 from service.common import log_handlers
 from flask_talisman import Talisman
+from flask_cors import CORS
 
 # Create Flask application
 app = Flask(__name__)
@@ -25,6 +26,8 @@ from service.common import error_handlers, cli_commands  # noqa: F401 E402
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
 
+CORS(app)
+
 app.logger.info(70 * "*")
 app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "*")
@@ -37,3 +40,4 @@ except Exception as error:  # pylint: disable=broad-except
     sys.exit(4)
 
 app.logger.info("Service initialized!")
+
